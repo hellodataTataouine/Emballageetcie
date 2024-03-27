651bc60a41b80dc5659e664d449ecd652659979f
HoudaTeyeb <teyebhouda1994@gmail.com>
2024-03-27 13:43:37 +0100

    mes produits

--- resources/views/frontend/default/pages/partials/home/category.blade.php
+++ resources/views/frontend/default/pages/partials/home/category.blade.php
@@ -1,6 +1,6 @@
 <section class="gshop-category-section bg-white pt-120 position-relative z-1 overflow-hidden">
-    <img src="{{ staticAsset('frontend/default/assets/img/shapes/bg-shape.png') }}" alt="bg shape"
-        class="position-absolute bottom-0 start-0 w-100 z--1">
+    <!-- <img src="{{ staticAsset('frontend/default/assets/img/shapes/bg-shape.png') }}" alt="bg shape"
+        class="position-absolute bottom-0 start-0 w-100 z--1"> -->
     <div class="container">
         <div class="gshop-category-box border-secondary rounded-3 bg-white">
             <div class="text-center section-title">
