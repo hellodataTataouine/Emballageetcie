e31e10337a6f30f6275108042c8449bdc91f64a1
HoudaTeyeb <teyebhouda1994@gmail.com>
2024-05-18 12:17:22 +0200

    lazysizes

---
+++ app/Http/Middleware/LazyLoadImagesMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class LazyLoadImagesMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        // Modify HTML response to include lazy loading attributes for images
+        $content = $response->getContent();
+        $lazyLoadedContent = $this->lazyLoadImages($content);
+
+        $response->setContent($lazyLoadedContent);
+
+        return $response;
+    }
+
+    protected function lazyLoadImages($content)
+    {
+        // Use regular expression to find image tags and add lazy loading attributes
+        $lazyLoadedContent = preg_replace('/<img(.*?)src="(.*?)"(.*?)>/', '<img$1src="$2" data-src="$2"$3 class="lazyload">', $content);
+
+        return $lazyLoadedContent;
+    }
+}
--- resources/views/frontend/default/pages/home.blade.php
+++ resources/views/frontend/default/pages/home.blade.php
@@ -58,6 +58,8 @@
 @endsection
 
 @section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" async></script>
+
     <script>
         "use strict";
 
