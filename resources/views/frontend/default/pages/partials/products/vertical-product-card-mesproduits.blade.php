2420fc4d6660e8409ede81a5d7251be640dab80c
Teyebhouda <teyebhouda1994@gmail.com>
2024-06-04 14:14:13 +0200

    children product

--- resources/views/frontend/default/pages/partials/products/vertical-product-card-mesproduits.blade.php
+++ resources/views/frontend/default/pages/partials/products/vertical-product-card-mesproduits.blade.php
@@ -79,7 +79,7 @@
             {{ localize('Ajouter au panier') }}
         </a>
     @else
-    @if ($product->parents()->count() > 0)
+    @if ($product->parents()->where('is_published', 1)->where('afficher', 1)->count() > 0)
     <a href="{{ route('products.show', $product->slug) }}" class="btn btn-outline-secondary btn-md border-secondary d-block mt-4">
         {{ __('Disponible en  :count références', ['count' => $product->parents()->where('is_published', 1)->where('afficher', 1)->count() + 1])  }}
     </a>
